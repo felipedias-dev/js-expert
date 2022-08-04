d1ff15c0af81a43e51079a4a79e5613634f4d0dd
Felipe Dias <felipedias.dev@gmail.com>
2022-08-04 00:09:44 -0300

    feat: :sparkles: add class inheritance

--- Module-2/prototype-chain/index.js
+++ Module-2/prototype-chain/index.js
@@ -80,3 +80,28 @@ assert.deepStrictEqual(manager.__proto__.__proto__.__proto__.__proto__.__proto__
 
 // -------
 console.log('-------');
+
+class T1 {
+  ping() { return 'ping' }
+}
+
+class T2 extends T1 {
+  pong() { return 'pong' }
+}
+
+class T3 extends T2 {
+  shoot() { return 'shoot' }
+}
+
+const t3 = new T3();
+
+console.log('t3 inherits null?', t3.__proto__.__proto__.__proto__.__proto__.__proto__ === null);
+console.log('t3.ping()', t3.ping());
+console.log('t3.pong()', t3.pong());
+console.log('t3.shoot()', t3.shoot());
+
+assert.deepStrictEqual(t3.__proto__, T3.prototype);
+assert.deepStrictEqual(t3.__proto__.__proto__, T2.prototype);
+assert.deepStrictEqual(t3.__proto__.__proto__.__proto__, T1.prototype);
+assert.deepStrictEqual(t3.__proto__.__proto__.__proto__.__proto__, Object.prototype);
+assert.deepStrictEqual(t3.__proto__.__proto__.__proto__.__proto__.__proto__, null);
