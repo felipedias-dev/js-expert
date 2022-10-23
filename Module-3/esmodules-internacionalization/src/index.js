7a32e573780d4c8c8979b86baecce2c1beae4adf
Felipe Dias <felipedias.dev@gmail.com>
2022-10-23 00:18:19 -0300

    refactor: :art: refactor terminal controller

--- Module-3/esmodules-internacionalization/src/index.js
+++ Module-3/esmodules-internacionalization/src/index.js
@@ -1,36 +1,8 @@
-import DraftLog from 'draftlog';
-import chalk from 'chalk';
-import chalkTable from 'chalk-table';
-import readLine from 'readline';
-import { stdin as input, stdout as output } from 'process';
-
 import database from '../database.json' assert { type: 'json' };
-import Person from './person.js';
-
-DraftLog(console).addLineListener(process.stdin);
+import TerminalController from './terminalController.js';
 
 const DEFAULT_LANG = 'pt-BR';
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: 'id', name: chalk.yellow('ID') },
-    { field: 'vehicles', name: chalk.magenta('Vehicles') },
-    { field: 'kmTraveled', name: chalk.red('Km Traveled') },
-    { field: 'from', name: chalk.green('From') },
-    { field: 'to', name: chalk.cyan('To') },
-  ]
-};
 
-const table = chalkTable(options, database.map(item => new Person(item).formatted(DEFAULT_LANG)));
-const print = console.draft(table);
+const terminalController = new TerminalController();
 
-print();
-
-const terminal = readLine.createInterface({
-  input,
-  output,
-});
-
-terminal.question('Qual é o seu nome? ', msg => {
-  console.log('msg', msg);
-})
+terminalController.initializeTerminal(database, DEFAULT_LANG);
---
+++ Module-3/esmodules-internacionalization/src/terminalController.js
@@ -0,0 +1,50 @@
+import DraftLog from 'draftlog';
+import chalk from 'chalk';
+import chalkTable from 'chalk-table';
+import readLine from 'readline';
+import { stdin as input, stdout as output } from 'process';
+
+import Person from './person.js';
+
+export default class TerminalController {
+  constructor() {
+    this.print = {};
+    this.data = {};
+  }
+
+  initializeTerminal(database, language) {
+    DraftLog(console).addLineListener(process.stdin);
+    
+    this.terminal = readLine.createInterface({
+      input,
+      output,
+    });
+
+    this.initializeTable(database, language);
+  }
+
+  initializeTable(database, language) {
+    const data = database.map(item => new Person(item).formatted(language));
+    const table = chalkTable(this.getTableOptions(), data);
+    
+    this.data = data;
+    this.print = console.draft(table);
+  }
+
+  question(msg = '') {
+    return new Promise(resolve => this.terminal.question(msg, resolve));
+  }
+
+  getTableOptions() {
+    return {
+      leftPad: 2,
+      columns: [
+        { field: 'id', name: chalk.yellow('ID') },
+        { field: 'vehicles', name: chalk.magenta('Vehicles') },
+        { field: 'kmTraveled', name: chalk.red('Km Traveled') },
+        { field: 'from', name: chalk.green('From') },
+        { field: 'to', name: chalk.cyan('To') },
+      ]
+    };
+  }
+}
