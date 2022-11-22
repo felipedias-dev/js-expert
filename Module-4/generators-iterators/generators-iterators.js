0d38a8b613ba8041abcbcab4715995b0cce948b2
Felipe Dias <felipedias.dev@gmail.com>
2022-11-22 18:34:15 -0300

    feat: :sparkles: add async iterators

--- Module-4/generators-iterators/generators-iterators.js
+++ Module-4/generators-iterators/generators-iterators.js
@@ -33,3 +33,38 @@ assert.deepStrictEqual(generator.next(), { value: undefined, done: true });
 
 assert.deepStrictEqual(Array.from(main()), ['Hello', '-', 'World', 200]);
 assert.deepStrictEqual([...main()], ['Hello', '-', 'World', 200]);
+
+// ---- async iterators
+const { readFile, stat, readdir } = require('fs/promises');
+
+function* promisified() {
+  yield readFile(__filename);
+  yield Promise.resolve('Hey, Dude!');
+}
+
+// console.log('promisified', [...promisified()]);
+
+// Promise.all([...promisified()]).then(results => console.log('promisified-results', results));
+
+// (async () => {
+//   for await (const item of promisified()) {
+//     console.log('for await', item.toString());
+//   }
+// })();
+
+async function* systemInfo() {
+  const file = await readFile(__filename);
+  yield { file: file.toString() };
+
+  const { size } = await stat(__filename);
+  yield { size };
+
+  const dir = await readdir(__dirname);
+  yield { dir };
+}
+
+(async () => {
+  for await (const item of systemInfo()) {
+    console.log('systemInfo', item);
+  }
+})();
